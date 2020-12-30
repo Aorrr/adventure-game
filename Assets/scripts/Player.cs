294a199b8dfa612d0e1de2704c66b3b87b7dc69b
nick <linanxi2024@outlook.com>
2020-12-30 11:11:43 +1100

    adds character animations, and adds a new map

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Config
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
+
+    // State
+    bool isAlive = true;
+
+    // cached component references
+    Rigidbody2D myRidigidBody;
+    Animator myAnimator;
+    BoxCollider2D myFeet;
+    CapsuleCollider2D myBodyCollider;
+    float gravityScaleAtStart;
+
+    // Message then methods 
+    void Start()
+    {
+        myRidigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRidigidBody.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAlive) { return; }
+        Die();
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // -1 to 1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRidigidBody.velocity.y);
+        myRidigidBody.velocity = playerVelocity;
+    }
+
+    private void Jump()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            myAnimator.SetBool("isJumping", true);
+            return;
+        } else
+        {
+            myAnimator.SetBool("isJumping", false);
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRidigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRidigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed)
+        {
+            if(myAnimator.GetBool("isJumping") == false) { myAnimator.SetBool("isRunning", true); }
+            transform.localScale = new Vector2(Mathf.Sign(myRidigidBody.velocity.x)*-1, 1f);
+        } else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+    }
+
+    
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
+        {
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            isAlive = false;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+
+    public void slowDown(float speed) {
+        runSpeed = speed;
+    }
+
+    public void speedUp(float speed) {
+        runSpeed = speed;
+    }
+}
---
+++ Assets/scripts/coinPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinPickUp : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int score = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
+        FindObjectOfType<GameSession>().AddToScore(score);
+        Destroy(gameObject);   
+    }
+}
