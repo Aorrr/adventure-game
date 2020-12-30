d0b33949ae1e508543c5b52c5122f4839bf82599
aliu4429 <aliu4429@uni.sydney.edu.au>
2020-12-30 18:50:53 +1100

    added simple light

---
+++ Assets/scripts/Light.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField] Player player;
+    Light2D parameters;
+
+    public float GetRadius()
+    {
+        return parameters.pointLightOuterRadius;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = player.transform.position;
+        parameters = GetComponent<Light2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lightPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = lightPos;
+        if(parameters.pointLightOuterRadius > 0)
+            parameters.pointLightOuterRadius -= 0.001f;
+    }
+}
---
+++ Assets/scripts/Shade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image shade;
+    Light light;
+    void Start()
+    {
+        shade = GetComponent<Image>();
+        light = FindObjectOfType<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shade.fillAmount = 1 - light.GetRadius() / 2.5f;
+    }
+}
