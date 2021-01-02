2f2ebb7b1965ae17817c38d985308855ea2a9853
nick <linanxi2024@outlook.com>
2021-01-02 18:22:11 +1100

    light bar function properly and changes camera setting

--- Assets/scripts/Light.cs
+++ Assets/scripts/Light.cs
@@ -6,6 +6,8 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class Light : MonoBehaviour
 {
     [SerializeField] Player player;
+    [SerializeField] float fadeSpeed;
+    float initialRadius;
     Light2D parameters;
 
     public float GetRadius()
@@ -13,12 +15,18 @@ public class Light : MonoBehaviour
         return parameters.pointLightOuterRadius;
     }
 
+    public float GetInitialRadius()
+    {
+        return initialRadius;
+    }
     // Start is called before the first frame update
     void Start()
     {
+       // parameters.pointLightOuterRadius = outerRadius;
         transform.position = player.transform.position;
         parameters = GetComponent<Light2D>();
-        
+        initialRadius = parameters.pointLightOuterRadius;
+
     }
 
     // Update is called once per frame
@@ -27,6 +35,6 @@ public class Light : MonoBehaviour
         Vector2 lightPos = new Vector2(player.transform.position.x, player.transform.position.y);
         transform.position = lightPos;
         if(parameters.pointLightOuterRadius > 0)
-            parameters.pointLightOuterRadius -= 0.001f;
+            parameters.pointLightOuterRadius -= 0.001f * fadeSpeed;
     }
 }
---
+++ Assets/scripts/lightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class lightController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Slider slider;
+    Light light;
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        light = FindObjectOfType<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = light.GetRadius() / light.GetInitialRadius();
+    }
+}
