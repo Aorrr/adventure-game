63aceeca74e3400cad4608e807ca78c7353dc975
nick <linanxi2024@outlook.com>
2021-01-05 13:29:26 +1100

    stops player when heavy attacking

---
+++ Assets/scripts/body.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class body : MonoBehaviour
+{
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void stop()
+    {
+        player.slowDown(0);
+    }
+
+    public void release()
+    {
+        player.speedUp(3);
+    }
+}
