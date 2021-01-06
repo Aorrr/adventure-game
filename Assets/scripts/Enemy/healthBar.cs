6a1b8014097cd4e0bfcdb97423e8899b76e10d13
nick <linanxi2024@outlook.com>
2021-01-06 14:18:28 +1100

    adds health bar

---
+++ Assets/scripts/Enemy/healthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBar : MonoBehaviour
+{
+    [SerializeField] Enemy AttachedEnemy;
+
+    Vector3 localScale;
+    float initialScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        initialScale = localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localScale.x = AttachedEnemy.GetHealthPercentage() * initialScale;
+        transform.localScale = localScale;
+        Debug.Log(AttachedEnemy.GetHealthPercentage());
+    }
+}
