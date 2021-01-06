dd7c66491a91b7543397be3b303ba63ec2d883c7
nick <linanxi2024@outlook.com>
2021-01-07 10:14:23 +1100

    betters enemy health bar rotation

--- Assets/scripts/Enemy/healthBar.cs
+++ Assets/scripts/Enemy/healthBar.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class healthBar : MonoBehaviour
 {
     [SerializeField] Enemy AttachedEnemy;
+    [SerializeField] GameObject Hpbar;
 
     Vector3 localScale;
     float initialScale;
+    Vector3 rotation;
+
     // Start is called before the first frame update
     void Start()
     {
         localScale = transform.localScale;
         initialScale = localScale.x;
+        rotation = Hpbar.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         localScale.x = AttachedEnemy.GetHealthPercentage() * initialScale;
+
+        if(AttachedEnemy.transform.localScale.x < 0)
+        {
+            rotation.x = -Mathf.Abs(rotation.x);
+            Hpbar.transform.localScale = rotation;
+        } else
+        {
+            rotation.x = Mathf.Abs(rotation.x);
+            Hpbar.transform.localScale = rotation;
+        }
+
         transform.localScale = localScale;
-        Debug.Log(AttachedEnemy.GetHealthPercentage());
     }
 }
