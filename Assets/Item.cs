59c43cc40bb4fd79a57fbbfc5226f5014af0a17e
aliu4429 <aliu4429@uni.sydney.edu.au>
2021-02-16 17:05:37 +1100

    level1

---
+++ Assets/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] Player player;
+    [SerializeField] string itemName;
+    [SerializeField] GameObject pickupEffect;
+    [SerializeField] float animationDuration;
+
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.ReceiveItem(itemName);
+        GameObject effect = Instantiate(pickupEffect, transform.position, transform.rotation);
+        Destroy(effect, animationDuration);
+        Destroy(gameObject);
+    }
+}
